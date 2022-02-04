53cb8f4bc3e91ffe95ea23cd68722e70c5440deb
Dileepa Bandara <80202913+dileepabandara@users.noreply.github.com>
2022-02-04 12:39:07 +0530

    Update 10_if_else_if_statement.dart

--- basic_dart/10_if_else_if_statement.dart
+++ basic_dart/10_if_else_if_statement.dart
@@ -3,13 +3,13 @@ var age = 24;
 var country = "Sri Lanka";
 
 void main() {
-  // if (name is! String) {
-  //   print("Name is not a INTEGER");
-  // } else if (age is int) {
-  //   print("Age is a INTEGER");
-  // } else if (age + 1 is int) {
-  //   print("Current age is ${age + 1}");
-  // } else {
-  //   print("Nothing is correct!");
-  // }
+  if (name is! String) {
+    print("Name is not a INTEGER");
+  } else if (age is int) {
+    print("Age is a INTEGER");
+  } else if (age + 1 is int) {
+    print("Current age is ${age + 1}");
+  } else {
+    print("Nothing is correct!");
+  }
 }
